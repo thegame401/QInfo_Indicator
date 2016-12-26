98e9ac68de3b745df528470cb2bb67f963cad0d9
thegame401 <mkungnaja01@gmail.com>
2016-12-26 21:31:38 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var initIndex = function()
+{
+	//ajaxGet('')
+}
+
+var ajaxGet = function(urlPath, onSuccessFn)
+{
+	$.ajax({
+        url: urlPath,
+        dataType: 'json',
+        success: onSuccessFn,
+        error: function (request, status, error) {
+            console.log(error);
+        }
+    });
+}
