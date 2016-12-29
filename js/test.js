f145d5f85d4f223622ea76866eeb6634c25340d9
Theerawut <Theerawutt53@gmail.com>
2016-12-29 20:54:59 +0700

    with test

--- js/test.js
+++ js/test.js
@@ -3,31 +3,41 @@ require(['jquery','query'],function($,
 
   var test_sets = [
     {  
+       'name':'TEST 1 GET educationchild',
        'db': 'educationchild',
        'view' : 'cid',
        'get' : {'start':['111'],include_doc:true,limit:1},
+    },
+    {  
+       'name':'TEST 2 LIST educationchild',
+       'db': 'educationchild',
+       'view' : 'cid',
        'list': {'start':['111'],'end':['112'],limit:10}
     }
   ];
+  
 
   var q = new query(); 
 
   var content = $('#content');
 
   test_sets.forEach(function(test) {
-    q.get(test.db,test.view,test.get,function(err,result) {
-      content.append('<p>Get: <b>'+test.db+'</b><br>');
-      content.append(q.toHTML(result.value.doc));
-      content.append('</p>');
-    });
-
-    q.list(test.db,test.view,test.list,function(err,result) {
-      content.append('<p>List :<b>'+test.db+'</b><br>');
-      result.forEach(function(item) {
-        content.append(q.toHTML(item.value));
+    if(test.get) {
+      q.get(test.db,test.view,test.get,function(err,result) {
+        content.append('<p>GET: <b>'+test.name+'</b><br>');
+        content.append(q.toHTML(result.value.doc));
+        content.append('</p>');
       });
-      content.append('</p>');
-    });
-    
+    }
+
+    if(test.list) {
+      q.list(test.db,test.view,test.list,function(err,result) {
+        content.append('<p>List :<b>'+test.name+'</b><br>');
+        result.forEach(function(item) {
+          content.append(q.toHTML(item.value));
+        });
+        content.append('</p>');
+      });
+    }
   });
 });
